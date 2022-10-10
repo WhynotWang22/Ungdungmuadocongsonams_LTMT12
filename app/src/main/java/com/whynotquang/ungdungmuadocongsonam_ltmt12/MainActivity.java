f68cd4c98574649290231c3269bfdf855c435a9f
Quang <nguoila524@gmail.com>
2022-10-10 16:40:45 +0700

    Quang up code

--- app/src/main/java/com/whynotquang/ungdungmuadocongsonam_ltmt12/MainActivity.java
+++ app/src/main/java/com/whynotquang/ungdungmuadocongsonam_ltmt12/MainActivity.java
@@ -3,24 +3,39 @@ package com.whynotquang.ungdungmuadocongsonam_ltmt12;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.whynotquang.ungdungmuadocongsonam_ltmt12.fragment.AccountFragment;
 import com.whynotquang.ungdungmuadocongsonam_ltmt12.fragment.CartFragment;
 import com.whynotquang.ungdungmuadocongsonam_ltmt12.fragment.CategoryFragment;
 import com.whynotquang.ungdungmuadocongsonam_ltmt12.fragment.HomeFragment;
 import com.whynotquang.ungdungmuadocongsonam_ltmt12.fragment.NotificationFragment;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView navigationView;
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        ////
         navigationView = findViewById(R.id.navigation);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new HomeFragment()).commit();
--- app/src/main/java/com/whynotquang/ungdungmuadocongsonam_ltmt12/fragment/HomeFragment.java
+++ app/src/main/java/com/whynotquang/ungdungmuadocongsonam_ltmt12/fragment/HomeFragment.java
@@ -1,66 +1,75 @@
 package com.whynotquang.ungdungmuadocongsonam_ltmt12.fragment;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.whynotquang.ungdungmuadocongsonam_ltmt12.MainActivity;
 import com.whynotquang.ungdungmuadocongsonam_ltmt12.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class HomeFragment extends Fragment {
+    private ImageSlider imageSlider;
+    private FirebaseFirestore database = FirebaseFirestore.getInstance();
+    private RecyclerView recyclerView_Home;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        recyclerView_Home = view.findViewById(R.id.recyclerview_home);
+
+        imageSlider = view.findViewById(R.id.imageSlider);
+        ///
+        ArrayList<SlideModel> slideModels = new ArrayList<>();
+
+        database.collection("Images").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            for (QueryDocumentSnapshot queryDocumentSnapshot: task.getResult()){
+                                slideModels.add(new SlideModel(queryDocumentSnapshot.getString("url"),ScaleTypes.FIT));
+                                imageSlider.setImageList(slideModels, ScaleTypes.FIT);
+                            }
+                        }
+                        else {
+                            Toast.makeText(getContext(), "Cant load images", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getContext(), "Cant load images", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+        return view;
     }
 }
