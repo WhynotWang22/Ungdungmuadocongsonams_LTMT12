b575dcc1ed45b3c910d80275e878f50e0a17cbec
Quang <nguoila524@gmail.com>
2022-10-10 23:13:38 +0700

    code giao dien man hinh thanh toan

---
+++ app/src/main/java/com/whynotquang/ungdungmuadocongsonam_ltmt12/activity/CardActivity.java
@@ -0,0 +1,16 @@
+package com.whynotquang.ungdungmuadocongsonam_ltmt12.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.whynotquang.ungdungmuadocongsonam_ltmt12.R;
+
+public class CardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card);
+    }
+}
