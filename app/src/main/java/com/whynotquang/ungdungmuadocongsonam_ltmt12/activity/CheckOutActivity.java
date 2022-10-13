78cc057dda2b117f0c0ec7ec38483a7aaae17fb6
Quân <quanminhfc@gmail.com>
2022-10-13 16:10:22 +0700

    update checkout payment

--- app/src/main/java/com/whynotquang/ungdungmuadocongsonam_ltmt12/activity/CheckOutActivity.java
+++ app/src/main/java/com/whynotquang/ungdungmuadocongsonam_ltmt12/activity/CheckOutActivity.java
@@ -3,18 +3,14 @@ package com.whynotquang.ungdungmuadocongsonam_ltmt12.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.RatingBar;
 
 import com.whynotquang.ungdungmuadocongsonam_ltmt12.R;
 
-public class ReviewActivity extends AppCompatActivity {
+public class CheckOutActivity extends AppCompatActivity {
 
-    RatingBar ratingBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_review);
-        ratingBar = findViewById(R.id.ratingbar);
-        ratingBar.setNumStars(5);
+        setContentView(R.layout.activity_check_out);
     }
 }
