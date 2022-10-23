22ea3cb7d733471e0f6682f4295f4fc6a270ca86
dattqph14839 <dattqph14839@fpt.edu.vn>
2022-10-23 19:57:37 +0700

    update

---
+++ app/src/main/java/com/whynotquang/ungdungmuadocongsonam_ltmt12/activity/Sign_Activity.java
@@ -0,0 +1,13 @@
+package com.whynotquang.ungdungmuadocongsonam_ltmt12.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sign_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign);
+    }
+}
