a804e0dc7fa31507f276151940f7c8a32ccf9ff3
Quân <quanminhfc@gmail.com>
2022-10-28 15:32:54 +0700

    fix code giao dien va chuc nang login va sign up

--- app/src/main/java/com/whynotquang/ungdungmuadocongsonam_ltmt12/activity/LoginActivity.java
+++ app/src/main/java/com/whynotquang/ungdungmuadocongsonam_ltmt12/activity/LoginActivity.java
@@ -5,18 +5,24 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.ybq.android.spinkit.sprite.Sprite;
+import com.github.ybq.android.spinkit.style.ThreeBounce;
 import com.whynotquang.ungdungmuadocongsonam_ltmt12.MainActivity;
 import com.whynotquang.ungdungmuadocongsonam_ltmt12.R;
 import com.whynotquang.ungdungmuadocongsonam_ltmt12.api.ApiService;
 import com.whynotquang.ungdungmuadocongsonam_ltmt12.model.User;
+
+import java.util.regex.Pattern;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -30,7 +36,9 @@ public class LoginActivity extends AppCompatActivity {
     Button button_login;
     String token = null;
     TextView tvDangkychuyenamn;
-    @SuppressLint("MissingInflatedId")
+
+    ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,7 +47,14 @@ public class LoginActivity extends AppCompatActivity {
         ed_pass = findViewById(R.id.ed_password_login);
         button_login = findViewById(R.id.btn_login);
         tvDangkychuyenamn = (TextView) findViewById(R.id.tv_dangkychuyenamn);
+        progressBar = (ProgressBar)findViewById(R.id.spin_kit_login_in);
+        Sprite threeBounce = new ThreeBounce();
+        progressBar.setIndeterminateDrawable(threeBounce);
+        progressBar.setVisibility(View.GONE);
+
         chuyenamnhinh();
+
+        getWindow().setStatusBarColor(getResources().getColor(R.color.bg_xam));
 
         SharedPreferences sp = LoginActivity.this.getApplicationContext().getSharedPreferences("Login", MODE_PRIVATE);
         String aaa = sp.getString("token","");
@@ -54,10 +69,24 @@ public class LoginActivity extends AppCompatActivity {
                 {
                     Toast.makeText(LoginActivity.this, "Vui lòng nhập thông tin", Toast.LENGTH_SHORT).show();
                     return;
+                } else if (!validateEmail(email))
+                {
+                    Toast.makeText(LoginActivity.this, "Vui lòng nhập email đúng định dạng", Toast.LENGTH_SHORT).show();
+                    return;
                 }
+                else if (password.length()<6)
+                {
+                    Toast.makeText(LoginActivity.this, "Mật khẩu trên 6 ký tự", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
                 postData(email,password);
             }
         });
+    }
+    private boolean validateEmail(String email) {
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+        return pattern.matcher(email).matches();
     }
 
     private void chuyenamnhinh() {
@@ -72,7 +101,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private void postData(String email, String password) {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.1.2:3000/api/auth/")
+                .baseUrl("http://192.168.10.52:3000/api/auth/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         ApiService apiService = retrofit.create(ApiService.class);
@@ -84,6 +113,7 @@ public class LoginActivity extends AppCompatActivity {
                     User userApi = response.body();
                     token = userApi.getToken();
                     if (token !=null){
+                        progressBar.setVisibility(View.GONE);
                         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                         startActivity(intent);
                         finishAffinity();
@@ -95,6 +125,9 @@ public class LoginActivity extends AppCompatActivity {
                         Ed.putString("token",userApi.getToken());
                         Ed.commit();
                     }
+                }else {
+                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(LoginActivity.this, "Đăng nhập không thành công", Toast.LENGTH_SHORT).show();
                 }
             }
 
--- app/src/main/java/com/whynotquang/ungdungmuadocongsonam_ltmt12/activity/Sign_Activity.java
+++ app/src/main/java/com/whynotquang/ungdungmuadocongsonam_ltmt12/activity/Sign_Activity.java
@@ -4,18 +4,24 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatCheckBox;
 
+import com.github.ybq.android.spinkit.sprite.Sprite;
+import com.github.ybq.android.spinkit.style.ThreeBounce;
 import com.whynotquang.ungdungmuadocongsonam_ltmt12.R;
 import com.whynotquang.ungdungmuadocongsonam_ltmt12.api.ApiService;
 import com.whynotquang.ungdungmuadocongsonam_ltmt12.model.User;
+
+import java.util.regex.Pattern;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -28,14 +34,12 @@ public class Sign_Activity extends AppCompatActivity {
     private EditText edthoten;
     private EditText edtEmail;
     private EditText edtSdt;
-    private EditText edtMatkhau;
+    private EditText edtMatkhau, ed_diachi;
     private TextView tv_dangnhap;
-    private AppCompatCheckBox chkLuuMatKhau;
     private Button btnDangki;
-    String token = null;
 
+    ProgressBar progressBar;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,41 +49,73 @@ public class Sign_Activity extends AppCompatActivity {
         edtEmail = (EditText) findViewById(R.id.edt_email);
         edtSdt = (EditText) findViewById(R.id.edtSdt);
         edtMatkhau = (EditText) findViewById(R.id.edtMatkhau);
-        chkLuuMatKhau = (AppCompatCheckBox) findViewById(R.id.chkLuuMatKhau);
         btnDangki = (Button) findViewById(R.id.btn_dangki);
         tv_dangnhap = findViewById(R.id.tv_dangnhapchuyenmanhinh);
+        ed_diachi = (EditText) findViewById(R.id.ed_diachi);
         chuyenmanhinh();
-        dangky();
-        SharedPreferences sp = Sign_Activity.this.getApplicationContext().getSharedPreferences("Signup",MODE_PRIVATE);
-        String qqqq = sp.getString("token","");
 
+        progressBar = (ProgressBar)findViewById(R.id.spin_kit_sign_up);
+        Sprite threeBounce = new ThreeBounce();
+        progressBar.setIndeterminateDrawable(threeBounce);
 
-    }
+        getWindow().setStatusBarColor(getResources().getColor(R.color.bg_xam));
+        progressBar.setVisibility(View.GONE);
 
-    private void dangky() {
         btnDangki.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 String full_name = edthoten.getText().toString().trim();
                 String phone_number = edtSdt.getText().toString().trim();
                 String email = edtEmail.getText().toString().trim();
                 String password = edtMatkhau.getText().toString().trim();
-                if (email.isEmpty() || password.isEmpty())
-                {
-                    Toast.makeText(Sign_Activity.this, "Khong duoc bo trong", Toast.LENGTH_SHORT).show();
+                String diachi = edtMatkhau.getText().toString().trim();
+
+                if (email.isEmpty() || password.isEmpty() || full_name.isEmpty() || diachi.isEmpty() || phone_number.isEmpty()) {
+                    Toast.makeText(Sign_Activity.this, "Không được để trống thông tin", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                postData(full_name,phone_number,email,password);
+//                else if (!isValidName(full_name)) {
+//                    Toast.makeText(Sign_Activity.this, "Tên không được sử dụng ký tự", Toast.LENGTH_SHORT).show();
+//                    return;
+//                }
+                else if (!validateEmail(email)) {
+                    Toast.makeText(Sign_Activity.this, "Email không đúng định dạng", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else if (!validatePhone(phone_number)) {
+                    Toast.makeText(Sign_Activity.this, "Số điện thoại phải là số", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else if (phone_number.length()<10) {
+                    Toast.makeText(Sign_Activity.this, "Số điện thoại phải trên 10 số", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else if (password.length() < 6) {
+                    Toast.makeText(Sign_Activity.this, "Mật khẩu phải dài hơn 6 ký tự", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+                postData(full_name, phone_number, email, password, diachi);
             }
         });
+    }
+    public boolean isValidName(String name) {
+        return Pattern.compile("/^[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]*$/").matcher(name).matches();
+    }
+    private boolean validateEmail(String email) {
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+        return pattern.matcher(email).matches();
+    }
+    private boolean validatePhone(String phone) {
+        Pattern pattern = Patterns.PHONE;
+        return pattern.matcher(phone).matches();
     }
 
     private void chuyenmanhinh() {
         tv_dangnhap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Sign_Activity.this,LoginActivity.class);
+                Intent intent = new Intent(Sign_Activity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
@@ -88,40 +124,37 @@ public class Sign_Activity extends AppCompatActivity {
     }
 
 
-    private void postData(String  full_name, String phone_number, String email, String password){
+    private void postData(String full_name, String phone_number, String email, String password, String diachi) {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.1.2:3000/api/auth/")
+                .baseUrl("http://192.168.10.52:3000/api/auth/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         ApiService apiSignup = retrofit.create(ApiService.class);
 
-        Call<User> call = apiSignup.postRegister(email,full_name,phone_number,password);
+        Call<User> call = apiSignup.postRegister(email, full_name, phone_number, password, diachi);
         call.enqueue(new Callback<User>() {
             @Override
             public void onResponse(Call<User> call, Response<User> response) {
-                if (response.isSuccessful()){
-                    User userAPi = response.body();
-                    token = userAPi.getToken();
-                    if (token !=null){
-                        Intent intent = new Intent(Sign_Activity.this, LoginActivity.class);
-                        startActivity(intent);
-                        finishAffinity();
-                        Toast.makeText(Sign_Activity.this, "Đăng ky thành công", Toast.LENGTH_SHORT).show();
-                        SharedPreferences sp= getSharedPreferences("register", MODE_PRIVATE);
-                        SharedPreferences.Editor Ed= sp.edit();
-                        Ed.putString("email",userAPi.getEmail() );
-                        Ed.putString("password",userAPi.get_id());
-                        Ed.putString("token",userAPi.getToken());
-                        Ed.putString("full_name",userAPi.get_id());
-                        Ed.putString("phone_number",userAPi.getToken());
-                        Ed.commit();
-                    }
+                if (response.isSuccessful()) {
+                    edthoten.setText("");
+                    edtEmail.setText("");
+                    edtMatkhau.setText("");
+                    edtEmail.setText("");
+                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(Sign_Activity.this, "Đăng ký thành công", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(Sign_Activity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finishAffinity();
+                } else {
+                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(Sign_Activity.this, "Đăng ký không thành công", Toast.LENGTH_SHORT).show();
                 }
             }
+
             @Override
             public void onFailure(Call<User> call, Throwable t) {
                 Toast.makeText(Sign_Activity.this, "loi api", Toast.LENGTH_SHORT).show();
             }
         });
     }
-    }
+}
