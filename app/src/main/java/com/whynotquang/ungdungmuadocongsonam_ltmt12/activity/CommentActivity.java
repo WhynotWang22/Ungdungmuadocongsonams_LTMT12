8e660ba6059f4776530aa316ef9eec4f4df3c62f
Dinh Quang <101507047+WhynotWang22@users.noreply.github.com>
2022-11-20 22:13:32 +0700

    updated

--- app/src/main/java/com/whynotquang/ungdungmuadocongsonam_ltmt12/activity/CommentActivity.java
+++ app/src/main/java/com/whynotquang/ungdungmuadocongsonam_ltmt12/activity/CommentActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,6 +29,7 @@ import com.whynotquang.ungdungmuadocongsonam_ltmt12.model.Products;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -39,16 +43,33 @@ public class CommentActivity extends AppCompatActivity {
     private TextView tv_avg;
     private TextView tv_dem_comments;
     private CommentAdapter commentAdapter;
+    private ProgressBar progress5;
+    private ProgressBar progress4;
+    private ProgressBar progress3;
+    private ProgressBar progress2;
+    private ProgressBar progress1;
     List<Comment> productCommentList;
+    List<Comment> productCommentList1 = new ArrayList<>();
+    List<Comment> productCommentList2 = new ArrayList<>();
+    List<Comment> productCommentList3 = new ArrayList<>();
+    List<Comment> productCommentList4 = new ArrayList<>();
+    List<Comment> productCommentList5 = new ArrayList<>();
     ImageButton btnback_comment;
     @Override
+    @SuppressLint({"MissingInflatedId", "LocalSuppress"})
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment);
+
         rc_comment = findViewById(R.id.rc_comment);
         tv_avg = findViewById(R.id.tv_avg);
         tv_dem_comments = findViewById(R.id.tv_dem_comments);
         btnback_comment = findViewById(R.id.btnback_comment);
+        progress5 = (ProgressBar) findViewById(R.id.progress_5);
+        progress4 = (ProgressBar) findViewById(R.id.progress_4);
+        progress3 = (ProgressBar) findViewById(R.id.progress_3);
+        progress2 = (ProgressBar) findViewById(R.id.progress_2);
+        progress1 = (ProgressBar) findViewById(R.id.progress_1);
         Intent intent = getIntent();
         id = intent.getStringExtra("id");
         getdataComment();
@@ -61,7 +82,6 @@ public class CommentActivity extends AppCompatActivity {
     }
 
     private void getdataComment() {
-
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext(),RecyclerView.VERTICAL,false);
         rc_comment.setLayoutManager(linearLayoutManager);
         Retrofit retrofit = new Retrofit.Builder()
@@ -80,6 +100,39 @@ public class CommentActivity extends AppCompatActivity {
                     tv_dem_comments.setText(response.body().getDem()  + " Bình Luận");
                     CommentAdapter commentAdapter = new CommentAdapter(productCommentList,getApplicationContext());
                     rc_comment.setAdapter(commentAdapter);
+                    for (int i = 0; i < productCommentList.size(); i++) {
+                        if (productCommentList.get(i).getRatingStar() == 1) {
+                            productCommentList1.add(productCommentList.get(i));
+                        } else if (productCommentList.get(i).getRatingStar() == 1) {
+                            if (productCommentList.get(i).getRatingStar() == 1) {
+                                productCommentList1.add(productCommentList.get(i));
+                            }
+                        } else if (productCommentList.get(i).getRatingStar() == 2) {
+                            if (productCommentList.get(i).getRatingStar() == 2) {
+                                productCommentList2.add(productCommentList.get(i));
+
+                            }
+                        } else if (productCommentList.get(i).getRatingStar() == 3) {
+                            if (productCommentList.get(i).getRatingStar() == 3) {
+                                productCommentList3.add(productCommentList.get(i));
+
+                            }
+                        } else if (productCommentList.get(i).getRatingStar() == 4) {
+                            if (productCommentList.get(i).getRatingStar() == 4) {
+                                productCommentList4.add(productCommentList.get(i));
+
+                            }
+                        } else if (productCommentList.get(i).getRatingStar() == 5) {
+                            if (productCommentList.get(i).getRatingStar() == 5) {
+                                productCommentList5.add(productCommentList.get(i));
+                            }
+                        }
+                        progress1.setProgress(productCommentList1.size());
+                        progress2.setProgress(productCommentList2.size());
+                        progress3.setProgress(productCommentList3.size());
+                        progress4.setProgress(productCommentList4.size());
+                        progress5.setProgress(productCommentList5.size());
+                    }
                     commentAdapter.notifyDataSetChanged();
                 }
                 else {
@@ -93,4 +146,5 @@ public class CommentActivity extends AppCompatActivity {
             }
         });
     }
+
 }
