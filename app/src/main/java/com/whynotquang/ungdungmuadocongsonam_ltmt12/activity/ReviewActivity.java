d2e0217d118560d2996167d24ee95be117850f2c
Dinh Quang <101507047+WhynotWang22@users.noreply.github.com>
2022-12-26 14:57:53 +0700

    fix 26/12

--- app/src/main/java/com/whynotquang/ungdungmuadocongsonam_ltmt12/activity/ReviewActivity.java
+++ app/src/main/java/com/whynotquang/ungdungmuadocongsonam_ltmt12/activity/ReviewActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -153,12 +154,13 @@ public class ReviewActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<Product> call, Response<Product> response) {
                 if (response.isSuccessful()) {
-                    progressBar.setVisibility(View.VISIBLE);
+                    progressBar.setVisibility(View.GONE);
                     edCmtReview.setText("");
                     Toast.makeText(ReviewActivity.this, "Comment Thành Công", Toast.LENGTH_SHORT).show();
-//                    Intent intent = new Intent(ReviewActivity.this, MainActivity.class);
-//                    startActivity(intent);
-//                    finishAffinity();
+                    Intent intent = new Intent(ReviewActivity.this, ChitietActivity.class);
+                    intent.putExtra("id",id);
+                    startActivity(intent);
+                    finishAffinity();
                 } else {
                     Toast.makeText(ReviewActivity.this, "Comment không thành công", Toast.LENGTH_SHORT).show();
                 }
